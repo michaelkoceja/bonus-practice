0a1c4ca3525a58522a0dcf9b3acee3cb4228a700
Michael Koceja <michael.koceja@gmail.com>
2017-04-24 16:35:12 -0700

    add prompt, add console command to pull the first/last letter in a sentence and capitalize it

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
 var sentence = prompt("Enter a sentence");
+
+console.log(sentence);
+console.log(sentence.length);
+console.log(sentence.charAt(0).toUpperCase());
+console.log(
+  sentence.charAt(0).toUpperCase() + sentence.charAt(sentence.length - 1).toUpperCase()
+);
